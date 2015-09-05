4ad353611317667db16c410f97e0ff6f5efce72e
drbean <drbean@users.noreply.github.com>
2015-09-05 13:04:47 +0000

    long_command_line_pandoc_invocation_lost_twice

---
+++ steve/textbook.sh
@@ -0,0 +1,35 @@
+pandoc                                                   \
+	-s -f markdown -t latex                          \
+	--template=textbook                              \
+	--variable=documentclass:memoir                  \
+	--variable=mainfont:"Linux Libertine O"          \
+	--variable=CJKmainfont:"WenQuanYi Zen Hei"       \
+	--variable=CJKmonofont='WenQuanYi Zen Hei Mono'  \
+	--toc --natbib                                   \
+	-o textbook.tex                                  \
+	metadata.yaml                                    \
+	--include-in-header=package.md                   \
+	--include-before-body=cover_page.md              \
+	--include-before-body=frontmatter.md             \
+	preface.md                                       \
+	toolbox.md                                       \
+	whiteout.md                                      \
+	plagiarism.md                                    \
+	conversation.md                                  \
+	mindmap.md                                       \
+	notebook.md                                      \
+	evaluation.md                                    \
+	google.md                                        \
+	metaphor.md                                      \
+	thinking.md                                      \
+	mainmatter.md                                    \
+	selfintro.md                                     \
+	actionresearch.md                                \
+	imrd.md                                          \
+	shopping.md                                      \
+	adventure.md                                     \
+	backmatter.md                                    \
+	adjectives.md                                    \
+	awl.md                                           \
+	bib.md                                           \
+	authors.md
