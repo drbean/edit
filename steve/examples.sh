741639283168f3f39027636e7abf1cb0eea2782c
drbean <drbean@users.noreply.github.com>
2015-10-31 10:25:55 +0000

    spirit_of_action_research

---
+++ steve/examples.sh
@@ -0,0 +1,8 @@
+# Essay examples
+
+## Which essay is closer to the idea of having an idea, carrying out the idea, reporting the process and evaluating the result.
+
+
+1.  I think making a plan to play is much easier than  finding something to do. So I make a plan to go somewhere. I usually like to take a trip by myself. That will make me fell relax and improve my self-confident. In order to I have no car that I usually take train to be my travel vehicle. Last weekend I planned to go to Taichung for visiting The Lavender Cottage. It's so easy to get there, that has the shuttle bus can take. There is a beautiful place that I will come again. Maybe after the midterm exam, it's a wonderful trip I had.
+
+2. I feel much pressure from the daily schedules arranged by teachers. I try to relax myself through exercises in order to release worries in my heart. I also enjoy eating delicious foods. They make me feel relaxing and satisfying. There is a saying, "a healthy diet plus exercise regularly can renew the soul of a man." This is a way to balance myself and also a way to maintain myself healthy.  My goals aren't ambitious to everyone, but those are the ways I am doing and keeping myself moving forward. I believe as long as I stay with the rules which I set for myself, I can accomplish the goals for sure.
