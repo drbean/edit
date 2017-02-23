c70b06ef82ac18233f6a4421bf7eeeaca6aac4dd
drbean <drbean@freeshell.org>
2017-02-23 20:00:27 +0800

    rendition_to_html.needs_template.remaining_latex.rough!

---
+++ steve/webbook.sh
@@ -0,0 +1,47 @@
+pandoc                                                   \
+	-f markdown -t html                              \
+	--standalone                                     \
+	--variable=documentclass:memoir                  \
+	--variable=papersize:b5paper                     \
+	--variable=mainfont:"Linux Libertine O"          \
+	--variable=CJKmainfont:"WenQuanYi Zen Hei"       \
+	--variable=CJKmonofont='WenQuanYi Zen Hei Mono'  \
+	--toc --natbib                                   \
+	-o webbook.html                                  \
+	metadata.yaml                                    \
+	--include-in-header=package.md                   \
+	--include-before-body=cover_page.md              \
+	--include-before-body=frontmatter.md             \
+	preface.md                                       \
+	toolbox.md                                       \
+	career.md                                        \
+	whiteout.md                                      \
+	plagiarism.md                                    \
+	conversation.md                                  \
+	mindmap.md                                       \
+	notebook.md                                      \
+	evaluation.md                                    \
+	google.md                                        \
+	metaphor.md                                      \
+	thinking.md                                      \
+	mainmatter.md                                    \
+	story.md                                         \
+	selfintro.md                                     \
+	actionresearch.md                                \
+	imrad.md                                         \
+	survey.md                                        \
+	over.md                                          \
+	shopping.md                                      \
+	adventure.md                                     \
+	trip.md                                          \
+	examples.md                                      \
+	design.md                                        \
+	critique.md                                      \
+	exercise.md                                      \
+	backmatter.md                                    \
+	peer.md                                          \
+	inclass.md                                       \
+	adjectives.md                                    \
+	awl.md                                           \
+	bib.md                                           \
+	authors.md
