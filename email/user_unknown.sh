38032ef3c05867503badbca7a3a400f95e0d2df5
drbean <drbean@freeshell.org>
2020-12-25 21:46:56 +0800

    function_for_bashrc?

--- email/user_unknown.sh
+++ email/user_unknown.sh
@@ -1,3 +1,9 @@
 sed -i.BAK -e '/jys@mail.ntust.edu.tw/d' */address.txt
 svn diff */address.txt
 svn ci -m 'jys@mail.ntust.edu.tw: User unknown'
+user_unknown () 
+{ 
+    sed -i.BAK -e "/^$1$/d" */address.txt;
+    svn diff */address.txt;
+    svn ci -m "$1: User unknown"
+}
